d6ce7648f0793f1d96025757c234d8279069b512
上原隆寛 <takahiro.steadtler@gmail.com>
2017-11-06 17:24:06 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,15 @@ class UsersController < ApplicationController
    before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
    before_action :correct_user,  only: [:edit, :update]
    before_action :admin_user,     only: :destroy
-  def show
-    @user = User.find(params[:id])
+
+    def show
+  @user = User.find(params[:id])
+  @microposts = @user.microposts.paginate(page: params[:page])
   end
 
-def index
-   @users = User.paginate(page: params[:page])
-end
+  def index
+     @users = User.paginate(page: params[:page])
+   end
 
   def new
     @user = User.new
@@ -48,26 +50,21 @@ end
 
   private
 
-  def user_params
-  params.require(:user).permit(:name, :email, :password,
-                               :password_confirmation)
-end
-    def logged_in_user
-    unless logged_in?
-      store_location
-     flash[:danger] = "Please log in"
-     redirect_to login_url
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
-  end
 
-  def correct_user
-@user = User.find(params[:id])
-redirect_to(root_url) unless current_user?(@user)
-  end
+    # beforeフィルター
 
-  # 管理者かどうか確認
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
-  
+    # 正しいユーザーかどうかを確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # 管理者かどうかを確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
